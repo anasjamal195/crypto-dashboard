ee3ac0e2cc00ad0c9179fcdb83a2f52cffc3082f
anas <anasj5749@gmail.com>
2025-01-22 01:14:32 +0500

    Max button added

--- app/Services/BinanceApiService.php
+++ app/Services/BinanceApiService.php
@@ -579,7 +579,7 @@ class BinanceApiService
 
     public static function fetchAvailableQuantity($symbol, $trader, $market = 'SPOT')
     {
-      
+
         $user = User::find($trader);
         $apiKey = $user->api_key;
         $apiSecret = $user->api_secret;
@@ -624,8 +624,8 @@ class BinanceApiService
             $balance = collect($response['balances'])->where('asset', $symbol)->first();
             return [
                 'asset' => $symbol,
-                'free' => floatval($balance['free']) ?? 0, // Available balance
-                'locked' => floatval($balance['locked']) ?? 0 // Balance in orders
+                'free' => $balance['free'] ? floatval($balance['free']) : 0, // Available balance
+                'locked' => $balance['locked'] ? floatval($balance['locked']) : 0 // Balance in orders
             ];
         } else {
             // Log or handle the error appropriately
