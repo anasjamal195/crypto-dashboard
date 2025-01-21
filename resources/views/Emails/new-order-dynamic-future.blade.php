ad654fca0bd9e3a6ce764634d80cd5d3b295718e
anas <anasj5749@gmail.com>
2025-01-21 21:43:52 +0500

    Trader setup for dynamic trading

--- resources/views/Emails/new-order-dynamic-future.blade.php
+++ resources/views/Emails/new-order-dynamic-future.blade.php
@@ -10,7 +10,8 @@
     <h3>Symbol: {{ $details['symbol'] ?? 'N/A' }}</h3>
     <h3>{!! isset($details['amount']) ? 'Trade Amount: ' . e($details['amount']) . ' USDT' : '' !!}</h3>
     <h3>Leverage: {{ $details['leverage'] ?? 'N/A' }}</h3>
-    <h3>Position Type: {{ $details['type'] ?? 'N/A' }}</h3>
+    <h3>Position: {{ $details['position'] == 'BUY'?'LONG':'SHORT';  }}</h3>
+    <h3>Position Type: {{ ucwords($details['type']) ?? 'N/A' }}</h3>
     <h3>orderId: {{ $details['orderId'] ?? 'N/A' }}</h3>
     <h3>Side: {{ $details['side'] ?? 'N/A' }}</h3>
     <h3>Price: {{ $details['price'] ?? 'N/A' }} USDT</h3>
