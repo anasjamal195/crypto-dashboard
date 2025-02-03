9d6e7a275a69254d6cd1ad01872f2d934ce9abec
anas <anasj5749@gmail.com>
2025-02-04 00:55:35 +0500

    Future Long and SHORT worker configured

--- app/Services/FutureLiveTrades/LiveTradeSHORTFutureServiceEXP1.php
+++ app/Services/FutureLiveTrades/LiveTradeSHORTFutureServiceEXP1.php
@@ -142,7 +142,7 @@ class LiveTradeSHORTFutureServiceEXP1
                 if ($highestPrice < $latestPrice) {
                     $highestPrice = $latestPrice;
                 } else if ($latestPrice < $highestPrice * 0.9991) {
-                    BinanceApiService::openMarketPositionLiveTrader($tradeInstance->symbol,$tradeInstance->buyPrice,$tradeInstance->position === 'LONG' ?'BUY':'SELL',$tradeInstance->leverage,$tradeInstance->tradeAcc);
+                    BinanceApiService::openMarketPositionLiveTrader($tradeInstance->symbol,$tradeInstance->buyPrice,$tradeInstance->position === 'LONG' ?'BUY':'SELL',$tradeInstance->leverage,$tradeInstance->tradeAccount);
                     $isLoop = false;
                 }
                 usleep(300000);
