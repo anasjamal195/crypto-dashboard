29b770d3e7dd2e6d8432cb7c52acfdd564c7db02
anas <anasj5749@gmail.com>
2025-02-07 11:39:27 +0500

    New Formula Implemented

--- app/Services/FutureLiveTrades/LiveTradeSHORTFutureServiceEXP1.php
+++ app/Services/FutureLiveTrades/LiveTradeSHORTFutureServiceEXP1.php
@@ -104,8 +104,8 @@ class LiveTradeSHORTFutureServiceEXP1
 
 
                     Log::info('FutureTraderShortEXP1: Support: ' . $supportResistanceContition);
-                    Log::notice('FutureTraderLongEXP1: MA Condition: ' . $maCondition);
-                    Log::error('FutureTraderLongEXP1: MA MACandleDistance: ' . $maCandleDistance);
+                    Log::info('FutureTraderLongEXP1: MA Condition: ' . $maCondition);
+                    Log::info('FutureTraderLongEXP1: MA MACandleDistance: ' . $maCandleDistance);
 
 
 
