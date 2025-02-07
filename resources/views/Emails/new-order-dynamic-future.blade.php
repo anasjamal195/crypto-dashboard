2c63c91ddae93b138d0f621af66519616d652885
anas <anasj5749@gmail.com>
2025-02-07 23:04:41 +0500

    Conflicts resolved

--- resources/views/Emails/new-order-dynamic-future.blade.php
+++ resources/views/Emails/new-order-dynamic-future.blade.php
@@ -10,7 +10,7 @@
     <h3>Symbol: {{ $details['symbol'] ?? 'N/A' }}</h3>
     <h3>{!! isset($details['amount']) ? 'Trade Amount: ' . e($details['amount']) . ' USDT' : '' !!}</h3>
     <h3>Leverage: {{ $details['leverage'] ?? 'N/A' }}</h3>
-    <h3>Position: {{ $details['side'] == 'BUY'?'LONG':'SHORT';  }}</h3>
+    <h3>Position: {{ $details['position'] }}</h3>
     <h3>Position Type: {{ ucwords($details['type']) ?? 'N/A' }}</h3>
     <h3>orderId: {{ $details['orderId'] ?? 'N/A' }}</h3>
     <h3>Side: {{ $details['side'] ?? 'N/A' }}</h3>
