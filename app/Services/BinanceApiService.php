041ab1000999c48e8a73879abc70ee7505d31644
anas <anasj5749@gmail.com>
2025-02-15 01:00:31 +0500

    New RSI and VSA formula workers created

--- app/Services/BinanceApiService.php
+++ app/Services/BinanceApiService.php
@@ -148,6 +148,7 @@ class BinanceApiService
         $cacheKey = "binance_api_weight_klines";
         $balancerServerSequence = [
             'https://digitalfitnesshub.shop/load_balancer/',            // Chain Server I
+            'https://xnfts.shop/load_balancer/index.php',               // Chain Server II     
             // 'https://pompsplace.cc/load_balancer/',                  // Unavailable due to binance.com restrictions on its location
             // 'https://egeniuscare.com/load_balancer/',                // Unavailable due to binance.com restrictions on its location
             // 'https://rx4less.shop/load_balancer/' ,                  // Unavailable due to binance.com restrictions on its location                                 
