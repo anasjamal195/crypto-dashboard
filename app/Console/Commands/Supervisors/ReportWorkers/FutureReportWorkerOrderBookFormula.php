4bc9bb61bbb8075e6731472016d4718436b576f3
anas <anasj5749@gmail.com>
2025-03-23 00:43:30 +0500

    Internal Trader setup in progress for order book

---
+++ app/Console/Commands/Supervisors/ReportWorkers/FutureReportWorkerOrderBookFormula.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands\Supervisors\ReportWorkers;
+
+use App\CommonHelpers;
+use App\Services\BinanceApiService;
+use App\Services\CoinReportService;
+use App\Services\MailerService;
+use App\Services\ReportServiceOrderBook\LongReportService as ReportServiceOrderBookLongReportService;
+use App\Services\ReportServiceOrderBook\ShortReportService as ReportServiceOrderBookShortReportService;
+use App\Services\SupportResistanceFakeBreakoutReport\LongReportService;
+use App\Services\SupportResistanceFakeBreakoutReport\ShortReportService;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class FutureReportWorkerOrderBookFormula extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:future-report-worker-order-book-formula';
+    public $interval;
+    public $limit;
+    public $rsiThreshold;
+    public $obvCandles;
+    public $obvLimit;
+    public $stochLimit;
+    public $targetProfit;
+    public $market;
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate Future Coins Report';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $formula = 'Order-Book (Basic: Short)';
+        DB::table('coin_reports')->where('market', 'FUTURE')->where('formula', $formula)->delete();
+
+        try {
+            ReportServiceOrderBookShortReportService::updateCoinReport(
+                '5m',
+                1000,
+                'FUTURE',
+                $formula,
+            );
+            // ReportServiceOrderBookLongReportService::updateCoinReport(
+            //     '5m',
+            //     1000,
+            //     'FUTURE',
+            //     $formula,
+            // );
+        } catch (\Exception $e) {
+            Log::error($e);
+        }
+    }
+}
