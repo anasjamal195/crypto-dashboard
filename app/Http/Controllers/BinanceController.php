0506eff4ad0c92883cf9ad5f75e5fccdde3f1e75
anas <anasj5749@gmail.com>
2025-03-26 03:06:57 +0500

    Multi thread trigger system setup done

--- app/Http/Controllers/BinanceController.php
+++ app/Http/Controllers/BinanceController.php
@@ -18,66 +18,7 @@ class BinanceController extends Controller
 {
     public function getCoinReport($market, Request $request)
     {
-        $account = 2;
         // =======Testing==========================
-        $workers = DB::table('workers')->all();
-        $openSymbols = DB::table('live_trades_future_results')
-            ->where('trade_acc', $account)
-            ->where('trade_status', 'open')
-            ->pluck('symbol');
-
-        // Subquery to get the latest snapshot entry per symbol with required conditions
-        $fiveMinutesAgo = Carbon::now()->subMinutes(5);
-
-        $latestSnapshots = DB::table('order_book_snapshots as obs1')
-            ->select(
-                'obs1.symbol',
-                DB::raw('MAX(obs1.snapshot_time) as latest_snapshot_time')
-            )
-            ->where('obs1.snapshot_time', '>=', $fiveMinutesAgo)
-            ->where('obs1.depth', 1000)
-            ->where('obs1.signal', 'SHORT')
-            ->where('obs1.short_strength', '>=', 8)
-            ->groupBy('obs1.symbol');
-
-        $triggers = DB::table('order_book_snapshots as obs2')
-            ->joinSub($latestSnapshots, 'latest_obs', function ($join) {
-                $join->on('obs2.symbol', '=', 'latest_obs.symbol')
-                    ->on('obs2.snapshot_time', '=', 'latest_obs.latest_snapshot_time');
-            })
-            ->join('trade_handler as th', function ($join) use ($account, $openSymbols) {
-                $join->on('obs2.symbol', '=', 'th.symbol')
-                    ->where('th.position', 'LONG')
-                    ->where('th.tradeAccount', $account)
-                    ->whereNotIn('th.symbol', $openSymbols)
-                    ->where('th.isWorkerDispatched', 0);
-            })
-            ->select(
-                'obs2.symbol',
-                'obs2.snapshot_time',
-                'obs2.resistance_levels',
-                'obs2.support_levels',
-                'obs2.signal',
-                'obs2.long_strength',
-                'obs2.short_strength',
-                'th.buyPrice',
-                'th.isWorkerDispatched',
-                'th.id as trade_handler_id',
-            )
-            ->get()->toArray();
-
-
-        // $packets = CommonHelpers::distributeEntriesToWorkers($triggers);
-
-
-        foreach ($triggers as $trigger) {
-        }
-
-
-        dd($workers);
-        // =======Testing==========================
-
-
         // Fetch all unique symbols from the database
         $interval = $request->interval;
         $query = DB::table('coin_reports')
