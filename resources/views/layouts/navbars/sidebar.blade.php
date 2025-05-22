95c55f3b66a5dbc25d7245e0da657a5bcf190572
anas <anasj5749@gmail.com>
2025-05-22 18:31:19 +0500

    Spot Setup done

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -9,12 +9,11 @@
         /* Adjust this value to get the desired indentation */
     }
 </style>
-<div class="sidebar" data="{{ \App\CommonHelpers::getMetaValue(auth()->user()->id, 'enable_long_multithread', 0) == 1 ? 'primary' : 'blue' }}">
+<div class="sidebar" data="{{ \App\CommonHelpers::getMetaValue(auth()->user()->id, 'enable_spot', 0) == 1 ? 'primary' : 'blue' }}">
     <div class="sidebar-wrapper">
         <div class="logo">
             <a href="{{ route('home') }}" class="simple-text logo-mini">{{ __('C') }}</a>
-            <a href="{{ route('home') }}" class="simple-text logo-normal">{{ __('Crypto Api Store') }}</a><br>
-            <a href="{{ route('home') }}" class="simple-text logo-mini">{{ \App\CommonHelpers::getMetaValue(auth()->user()->id, 'enable_long_multithread', 0) == 1 ? 'Futures' : 'Spot' }}</a>
+            <a href="{{ route('home') }}" class="simple-text logo-normal">{{ __('Crypto Api Store') }}</a>
         </div>
         {{-- 5m-CandleSticks Parent Tab --}}
         <ul class="nav">
