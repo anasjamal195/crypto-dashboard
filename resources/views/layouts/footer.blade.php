d898c0359d544ad7dfa7339b65dad96e9f75fd4f
anas <anasj5749@gmail.com>
2025-08-06 00:50:52 +0500

    Checks added for closing live

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,38 +6,42 @@
     justify-content: space-between;
 ">
 
-        @php
-            $gitInfo = \App\CommonHelpers::getLastGitCommit(); // Replace with your actual controller/class
-        @endphp
 
-        @if ($gitInfo['success'])
-            <div class="mx-5 d-flex align-items-center justify-content-center text-muted">
-                <i class="tim-icons icon-git-branch mr-2"></i>
-                <span class="git-commit-text">
-                    <code class="commit-hash-mini">{{ $gitInfo['message'] }}</code>
-                    <span class="mx-2">•</span>
-                    <small>{{ \Carbon\Carbon::parse($gitInfo['date'])->diffForHumans() }}</small>
-                    @if ($gitInfo['author'])
+        @if (auth()->user() && auth()->user()->role !== 'analyst')
+            @php
+                $gitInfo = \App\CommonHelpers::getLastGitCommit(); // Replace with your actual controller/class
+            @endphp
+
+            @if ($gitInfo['success'])
+                <div class="mx-5 d-flex align-items-center justify-content-center text-muted">
+                    <i class="tim-icons icon-git-branch mr-2"></i>
+                    <span class="git-commit-text">
+                        <code class="commit-hash-mini">{{ $gitInfo['message'] }}</code>
                         <span class="mx-2">•</span>
-                        <small>{{ explode(' <', $gitInfo['author'])[0] }}</small>
-                    @endif
-                </span>
-                <a href="{{ route('deploy-git-commit') }}" class="btn btn-sm btn-outline-primary ml-2"
-                    onclick="return confirm('Are you sure you want to deploy?')" title="Deploy">
-                    <i class="tim-icons icon-cloud-upload-94 mr-1"></i>
-                    Deploy
-                </a>
-            </div>
-        @else
-            <div class="text-center text-muted">
-                <i class="tim-icons icon-git-branch mr-1"></i>
-                <small>No git info available</small>
-                <a href="{{ route('deploy-git-commit') }}" class="btn btn-sm btn-outline-primary ml-2"
-                    onclick="return confirm('Are you sure you want to deploy?')" title="Deploy">
-                    <i class="tim-icons icon-cloud-upload-94 mr-1"></i>
-                    Deploy
-                </a>
-            </div>
+                        <small>{{ \Carbon\Carbon::parse($gitInfo['date'])->diffForHumans() }}</small>
+                        @if ($gitInfo['author'])
+                            <span class="mx-2">•</span>
+                            <small>{{ explode(' <', $gitInfo['author'])[0] }}</small>
+                        @endif
+                    </span>
+                    <a href="{{ route('deploy-git-commit') }}" class="btn btn-sm btn-outline-primary ml-2"
+                        onclick="return confirm('Are you sure you want to deploy?')" title="Deploy">
+                        <i class="tim-icons icon-cloud-upload-94 mr-1"></i>
+                        Deploy
+                    </a>
+                </div>
+            @else
+                <div class="text-center text-muted">
+                    <i class="tim-icons icon-git-branch mr-1"></i>
+                    <small>No git info available</small>
+                    <a href="{{ route('deploy-git-commit') }}" class="btn btn-sm btn-outline-primary ml-2"
+                        onclick="return confirm('Are you sure you want to deploy?')" title="Deploy">
+                        <i class="tim-icons icon-cloud-upload-94 mr-1"></i>
+                        Deploy
+                    </a>
+                </div>
+            @endif
+
         @endif
 
 
