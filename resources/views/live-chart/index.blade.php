b59202077312e4178ac12f30a9a35cf17c3cb66e
anas <anasj5749@gmail.com>
2025-09-17 16:12:54 +0500

    Live chart preview added

--- resources/views/live-chart/index.blade.php
+++ resources/views/live-chart/index.blade.php
@@ -1,17 +1,21 @@
-<div>
-    <x-candlestick-chart :data="$data" symbol="{{ $symbol }}" interval="{{ $interval }}" :indicators="[
-            // 'ma7',
-            // 'ma14',
-            // 'ma25',
-            // 'ma99',
-            // 'bb',
-            // 'volume',
-            // 'rsi6',
-            // 'stoch_rsi',
-            // 'macd_hist',
-            // 'mfi',
-            // 'adx',
-            // 'sar',
-        ]"
-        :markers="$labelPlots" :trades="$zonePlots" :lines="$linePlots" />
-</div>
+@extends('layouts.app')
+
+@section('content')
+    <div>
+        <x-candlestick-chart :data="$data" symbol="{{ $symbol }}" interval="{{ $interval }}" :indicators="[
+                // 'ma7',
+                // 'ma14',
+                // 'ma25',
+                // 'ma99',
+                // 'bb',
+                // 'volume',
+                // 'rsi6',
+                // 'stoch_rsi',
+                // 'macd_hist',
+                // 'mfi',
+                // 'adx',
+                // 'sar',
+            ]"
+            :markers="$labelPlots" :trades="$zonePlots" :lines="$linePlots" />
+    </div>
+@endsection
